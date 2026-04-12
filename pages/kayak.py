3831fae1cf6befe0531acd197a9315a49e9a5e02
willrichmond <will@baseoperations.com>
2026-04-12 13:22:03 -0600

    river data

--- pages/kayak.py
+++ pages/kayak.py
@@ -1,8 +1,19 @@
 import streamlit as st
+import polars as pl
+from data.kayak import section_list, gauge_list,river_list
 
 st.title("📊 Page One")
 st.write("This is page one.")
 
-name = st.text_input("Enter your name")
-if name:
-    st.success(f"Hello, {name}!")
+section = pl.DataFrame(section_list)
+gauge = pl.DataFrame(gauge_list)
+river = pl.DataFrame(river_list)
+
+st.subheader("Sections")
+st.dataframe(section)
+
+st.subheader("Gauges")
+st.dataframe(gauge)
+
+st.subheader("Rivers")
+st.dataframe(river)
