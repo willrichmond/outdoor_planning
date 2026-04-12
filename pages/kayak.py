fec171e124d5fce043d0435494b763e24a6a45ec
willrichmond <will@baseoperations.com>
2026-04-12 13:51:42 -0600

    fixes

--- pages/kayak.py
+++ pages/kayak.py
@@ -1,13 +1,30 @@
 import streamlit as st
 import polars as pl
+from utils.kayak_utils import get_gauge_data, get_clean_gauge_data, get_kayaking_levels, get_current_river_levels
 from data.kayak import section_list, gauge_list,river_list
 
 st.title("📊 Page One")
 st.write("This is page one.")
 
-section = pl.DataFrame(section_list)
-gauge = pl.DataFrame(gauge_list)
-river = pl.DataFrame(river_list)
+@st.cache_data
+def load_static_data():
+    return pl.DataFrame(section_list), pl.DataFrame(gauge_list), pl.DataFrame(river_list)
+
+
+@st.cache_data(ttl=3600)
+def run_apis():
+    gauge_data = get_gauge_data(pl.DataFrame(gauge_list))
+    clean_gauge_data = get_clean_gauge_data(gauge_data)
+    kayaking_levels = get_kayaking_levels(clean_gauge_data, value_type="flow_cfs")
+    current_river_levels = get_current_river_levels(kayaking_levels)
+
+    return kayaking_levels, current_river_levels
+
+with st.spinner("Fetching river levels..."):
+    kayaking_levels, current_river_levels = run_apis()
+
+
+section, gauge, river = load_static_data()
 
 st.subheader("Sections")
 st.dataframe(section)
@@ -17,3 +34,7 @@ st.dataframe(gauge)
 
 st.subheader("Rivers")
 st.dataframe(river)
+
+st.subheader("Current River Levels")
+st.dataframe(kayaking_levels)
+st.dataframe(current_river_levels)
