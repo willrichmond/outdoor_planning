7f5209b0e55e51a34205c1522625a5abed26c12e
willrichmond <will@baseoperations.com>
2026-04-13 10:42:49 -0600

    fixes

--- app.py
+++ app.py
@@ -7,3 +7,13 @@ st.set_page_config(
 
 st.title("🏠 Home")
 st.write("Welcome! Use the sidebar to navigate between pages.")
+
+
+# To-Dos
+# Add flow levels
+# Split guages into separate for NOAA only
+# Fix the API calls (payette at cascade dam)
+# Update the formulas for section_id
+# Join the section names
+# Create a flow function for level & boat type
+# Fix the charts
